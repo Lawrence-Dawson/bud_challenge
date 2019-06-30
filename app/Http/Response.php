7eb562f7aecccf0e14b8ff58bb8e706dee42fac0
Lawrence Dawson <ld@mettrr.com>
2019-06-30 18:37:54 +0100

    make test for getting response body pass

--- app/Http/Response.php
+++ app/Http/Response.php
@@ -5,14 +5,21 @@ namespace App\Http;
 class Response
 {
     private $status;
+    private $body;
 
-    public function __construct(int $status)
+    public function __construct(int $status, $body = '')
     {
         $this->status = $status;
+        $this->body = $body;
     }
 
     public function getStatus(): int
     {
         return $this->status;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
     }
 }
