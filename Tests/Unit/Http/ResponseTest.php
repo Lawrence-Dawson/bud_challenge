c41545fc2916f80446ad6f580a8550b8741995bc
Lawrence Dawson <ld@mettrr.com>
2019-06-30 18:36:18 +0100

    add test for getting response body

--- Tests/Unit/Http/ResponseTest.php
+++ Tests/Unit/Http/ResponseTest.php
@@ -7,11 +7,20 @@ use PHPUnit\Framework\TestCase;
 
 class ResponseTest extends TestCase
 {
-    public function testItCanSetResponseStatus()
+    public function testItCanGetResponseStatus()
     {
         $status = 200;
         $response = new Response($status);
 
         $this->assertEquals($status, $response->getStatus());
+    }
+
+    public function testItCanGetResponseBody()
+    {
+        $status = 200;
+        $body = 'foo is bar';
+        $response = new Response($status, $body);
+
+        $this->assertEquals($body, $response->getBody());
     }
 }
