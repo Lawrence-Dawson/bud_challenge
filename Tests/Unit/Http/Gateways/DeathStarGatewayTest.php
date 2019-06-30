814bae43d35d2775b14efdefd8471d289119bbcc
Lawrence Dawson <ld@mettrr.com>
2019-06-30 21:50:20 +0100

    add test for destroying deathstar

--- Tests/Unit/Http/Gateways/DeathStarGatewayTest.php
+++ Tests/Unit/Http/Gateways/DeathStarGatewayTest.php
@@ -58,4 +58,50 @@ class DeathStarGatewayTest extends TestCase
             'Authorization' => 'Bearer ' . $responseBody['access_token']
         ]);
     }
+
+    public function testItCanDestroyDeathstar()
+    {
+        $client = Mockery::mock(Client::class);
+
+        $responseBody = [
+            'access_token' => 'e31a726c4b90462ccb7619e1b9d8u8d87d87d878d8d',
+            'expires_id' => 99999999999,
+            'token_type' => 'Bearer',
+            'scope' => 'TheForce'
+        ];
+
+        $authResponse = $this->createResponse(200, [], $responseBody);
+
+        $cert = 'certificate.pem';
+
+        $client->expects()
+            ->request('POST', 'https://death.star.api/token', [
+                'headers' => [],
+                'cert' => $cert,
+                'body' => json_encode([
+                    'Client secret' => $this->configs['death_star_secret'],
+                    'Client ID' => $this->configs['death_star_id'],
+                ])
+            ])
+            ->once()
+            ->andReturns($authResponse);
+
+        $destroyResponse = $this->createResponse(200, [], [
+            'success' => true,
+            'message' => 'exhaust deleted.'
+        ]);
+            
+        $client->expects()
+            ->request('DELETE', 'https://death.star.api/reactor/exhaust/1', [
+                'headers' => [],
+                'cert' => $cert,
+                'X-Torpedoes' => 2,
+                'body' => json_encode([])
+            ])
+            ->once()
+            ->andReturns($destroyResponse);
+
+        $gateway = new DeathStarGateway($client);
+        $gateway->destroy();
+    }
 }
